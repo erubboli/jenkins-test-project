1cac3c674b3a705df1d0ff221d482d872a5b624e
Enrico Rubboli <rubboli@gmail.com>
2014-06-19 17:16:41 +0200

    add a complicated method

--- app/models/untested_model.rb
+++ app/models/untested_model.rb
@@ -7,4 +7,13 @@ class UntestedModel < ActiveRecord::Base
   def this_is_tested
     true
   end
+
+  def this_is_a_complicated_method
+    2 + 2 
+    tot = 100
+    %w{f10oo b40ar}.each do |label|
+      tot += label.to_i 
+    end
+    "#{tot} - #{['a','b','v'].join('/')}"
+  end
 end
